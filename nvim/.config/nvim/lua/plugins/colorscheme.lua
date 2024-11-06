ed6af518450be309f2f189bc28bdb0ac30b3a59f
pedrohhope <pvpswapedro123@gmail.com>
2024-11-06 17:40:13 -0300

    first commit

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "maxmx03/dracula.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
